fa332f9d470391057752abb3dc4bff18e5478c1d
Manoj Talagadadeevi <manoj.talagadadeevi@ey.com>
2018-01-15 13:36:48 -0500

    Make sure the grade prop was passed by requiring it. For other props, we defined default values so the component works even if no custom data is passed

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import Rating from './components/Rating'
 
 new Vue({
   el: '#app',
-  template: '<Rating :grade="3" :maxStars="5" :hasCounter="true"/>',
+  template: '<Rating :grade="3"/>',
   components: { Rating }
 })
