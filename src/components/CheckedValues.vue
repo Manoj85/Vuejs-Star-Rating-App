db3e37662fea9138b77777a4507453202cc0f52c
Manoj Talagadadeevi <manoj.talagadadeevi@ey.com>
2018-01-31 22:21:13 -0500

    Add component `Checked Values`

---
+++ src/components/CheckedValues.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="checkboxValues">
+    <h3> Checkbox Values </h3>
+
+    <span v-for="option in options">
+      <input type="checkbox" :id="option.value" :value="option.value" v-model="checkedNames">
+      <label for="option.value"> {{option.value}}</label>
+    </span>
+
+    <br>
+    <span>Checked names: {{ checkedNames }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "checked-values",
+    data() {
+      return {
+        checkedNames: [],
+        options: [
+          {value: 'John'},
+          {value: 'Paul'},
+          {value: 'George'},
+          {value: 'Ringo'}
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  body {
+    font-family: 'Bitter', serif;
+  }
+
+  #checkboxValues {
+    text-align: center;
+    padding-bottom: 70px;
+    max-width: 360px;
+    font-size: 16px;
+    margin: 0 auto;
+    display: table;
+    line-height: 2em;
+  }
+
+  label {
+    padding-right: 10px;
+  }
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,19 +2,27 @@ import Vue from 'vue'
 import Rating from './components/Rating'
 import BookBasic from './components/BookBasic'
 import BookContentTyper from './components/BookContentTyper'
+import CheckedValues from './components/CheckedValues'
 
 new Vue({
   el: '#app',
   template: `<div>
       <Rating :grade="3"/>
       <hr/>
+      
       <BookBasic />
       <hr/>
+      
       <BookContentTyper />
+      <hr/>
+      
+      <CheckedValues />
+      <hr/>
   </div>`,
   components: {
     Rating,
     BookBasic,
-    BookContentTyper
+    BookContentTyper,
+    CheckedValues
   }
 })
