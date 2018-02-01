d0cebf15b9ee27a979ab0f83577c3f926745a7e7
Manoj Talagadadeevi <manoj.talagadadeevi@ey.com>
2018-01-31 19:13:29 -0500

    Added two components - `Book` and `BookContentTyper`

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import Rating from './components/Rating'
+import BookBasic from './components/BookBasic'
+import BookContentTyper from './components/BookContentTyper'
 
 new Vue({
   el: '#app',
-  template: '<Rating :grade="3"/>',
-  components: { Rating }
+  template: `<div>
+      <Rating :grade="3"/>
+      <hr/>
+      <BookBasic />
+      <hr/>
+      <BookContentTyper />
+  </div>`,
+  components: {
+    Rating,
+    BookBasic,
+    BookContentTyper
+  }
 })
